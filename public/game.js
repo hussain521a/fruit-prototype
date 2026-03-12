ce38e2b6ddb2d9fc88c9b27a7082b268c5ed063d
Hussain A <hussain521ali@gmail.com>
2026-03-12 14:11:19 -0400

    Adjustments to npc movement and added boundaries

--- public/game.js
+++ public/game.js
@@ -7,15 +7,15 @@ const roomId = prompt("Enter room code for this game:");
 socket.emit("joinRoom", { roomId, role: "host" });
 
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: { default: "arcade" },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: { default: "arcade" },
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
 };
 
 const game = new Phaser.Game(config);
@@ -25,61 +25,63 @@ let npcs = [];
 
 function preload(){
 
-  this.load.image("npc","assets/npc.png");
-  this.load.image("player","assets/player.png");
+	this.load.image("npc","assets/npc.png");
+	this.load.image("player","assets/player.png");
 
 }
 
 function create(){
 
-  player = this.add.sprite(400,300,"player");
+	player = this.add.sprite(400,300,"player");
 
-  // create npc crowd
-  for(let i=0;i<20;i++){
+	// create npc crowd
+	for(let i=0;i<20;i++){
 
-    let npc = this.add.sprite(
-      Phaser.Math.Between(100,700),
-      Phaser.Math.Between(100,500),
-      "npc"
-    );
+		let npc = this.add.sprite(
+			Phaser.Math.Between(100,700),
+			Phaser.Math.Between(100,500),
+			"npc"
+		);
 
-    npc.speedX = Phaser.Math.Between(-1,1);
-    npc.speedY = Phaser.Math.Between(-1,1);
+		npc.speedX = Phaser.Math.Between(-2,2);
+		npc.speedY = Phaser.Math.Between(-2,2);
 
-    npcs.push(npc);
-  }
+		npcs.push(npc);
+	}
 
-  socket.on("updateThief",(data)=>{
-    player.x = data.x;
-    player.y = data.y;
-  });
+	socket.on("updateThief",(data)=>{
+		player.x = data.x;
+		player.y = data.y;
+	});
 
 }
 
 function update(){
 
-  npcs.forEach(npc=>{
+	npcs.forEach(npc=>{
+		// Move NPC
+		npc.x += npc.speedX;
+		npc.y += npc.speedY;
+		// Keep NPC in bounds
+		npc.x = Phaser.Math.Clamp(npc.x, 0, 800);
+		npc.y = Phaser.Math.Clamp(npc.y, 0, 600);
+		// Randomly change direction
+		if(Math.random() < 0.003){
+			npc.speedX = Phaser.Math.Between(-2,2);
+			npc.speedY = Phaser.Math.Between(-2,2);
+		}
 
-    npc.x += npc.speedX;
-    npc.y += npc.speedY;
-
-    if(Math.random() < 0.01){
-      npc.speedX = Phaser.Math.Between(-1,1);
-      npc.speedY = Phaser.Math.Between(-1,1);
-    }
-
-  });
-
-  depthSort();
-
+	});
+	
+	depthSort();
 }
-
+// Sorts sprites by their y value to create a depth effect with overlapping
 function depthSort(){
 
-  const sprites = [player,...npcs];
+	const sprites = [player,...npcs];
 
-  sprites.forEach(sprite=>{
-    sprite.depth = sprite.y;
-  });
+	sprites.forEach(sprite=>{
+		sprite.depth = sprite.y;
+	});
 
 }
